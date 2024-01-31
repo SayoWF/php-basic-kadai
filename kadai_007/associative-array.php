3a2a80c2532a185e66e45aa432f0a860c117ec42
SayoWF <sayowaki@gmail.com>
2024-01-31 13:54:01 +0900

    kadai_007追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP_kadai_008</title>
+</head>
+
+<body>
+    <?php
+    $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    //連想配列の値とキーを出力
+    print_r($vegetable_data);
+    ?>
+</body>
+
+</html>
